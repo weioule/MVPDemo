e9610639c76276586b4687941a6bb0317c5d2672
weioule <weioule@peilian.com>
2018-12-11 10:52:43 +0800

    add bat info

---
+++ batinfo/vipweike.bat
@@ -0,0 +1,42 @@
+set APPVER="1.1.0"
+
+e:
+
+cd %cd%
+
+ echo  小米
+    call ant -DAPPVER=%APPVER% -DUMENG="xiaomi" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  华为
+    call ant -DAPPVER=%APPVER% -DUMENG="huawei" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  应用宝
+    call ant -DAPPVER=%APPVER% -DUMENG="tencent" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  百度
+    call ant -DAPPVER=%APPVER% -DUMENG="baidu" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  360助手
+    call ant -DAPPVER=%APPVER% -DUMENG="360" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  豌豆荚
+    call ant -DAPPVER=%APPVER% -DUMENG="wandoujia" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  魅族
+    call ant -DAPPVER=%APPVER% -DUMENG="meizu" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  oppo
+    call ant -DAPPVER=%APPVER% -DUMENG="oppo" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  vivo
+    call ant -DAPPVER=%APPVER% -DUMENG="vivo" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  搜狗
+    call ant -DAPPVER=%APPVER% -DUMENG="sogou" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  安智
+    call ant -DAPPVER=%APPVER% -DUMENG="anzhi" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  木蚂蚁
+    call ant -DAPPVER=%APPVER% -DUMENG="mumayi" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  91助手
+    call ant -DAPPVER=%APPVER% -DUMENG="91zhushou" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  机锋
+    call ant -DAPPVER=%APPVER% -DUMENG="jifeng" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  优亿
+    call ant -DAPPVER=%APPVER% -DUMENG="youyi" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  N多网
+    call ant -DAPPVER=%APPVER% -DUMENG="nduowang" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  联想
+    call ant -DAPPVER=%APPVER% -DUMENG="lenovo" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
+    echo  淘宝
+    call ant -DAPPVER=%APPVER% -DUMENG="taobao" -DAPKPATH="vipweike" -DAPKNAME="vipweike"
